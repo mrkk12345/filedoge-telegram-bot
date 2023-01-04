9eac052c0718873530574e7c31461bf62f538eb3
mrkk12345 <111275231+mrkk12345@users.noreply.github.com>
2023-01-04 22:57:46 +0530

    Update index.js

--- index.js
+++ index.js
@@ -40,7 +40,10 @@ async function fileDownloader(url, fileName) {
   const writer = fs.createWriteStream(outputLocationPath);
 
   let doneDownloading = false;
-  await axios
+  await axios({
+  maxContentLength: Infinity,
+  maxBodyLength: Infinity,
+  })
     .get(url, { responseType: "stream" })
     .then((response) => {
       return new Promise((resolve, reject) => {
